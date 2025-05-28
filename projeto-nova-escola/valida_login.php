429d0286e1be1b3a4982a40f71adcf61f12da97e
tatianetinoco <tatianeperr@gmail.com>
2025-05-27 21:39:05 -0300

    Validação de Login

--- projeto-nova-escola/valida_login.php
+++ projeto-nova-escola/valida_login.php
@@ -1,4 +1,30 @@
 <?php
-require_once "adm/conexao.php";
+
+if(isset($_POST["acao"])){
+
+    // Importando arquivo conexao.php
+    require_once("adm/conexao.php");
+
+    // Obtendo valores digitados no formulário
+    $usuario = $_POST["usuario"];
+    $senha   = $_POST["senha"];
+
+    // Verificar se existe na base de dados
+    $sql = "SELECT * FROM pessoas WHERE pessoas.cpf =:login and pessoas.senha = :senha";
+    $comando = $pdo -> prepare($sql);
+    $comando -> bindParam(":login",$usuario);
+    $comando -> bindParam(":senha",$senha);
+    $comando -> execute();
+
+    if($comando -> rowCount()==1){
+        echo " Existe um usuário em nossa base de dados ";
+        echo ($usuario);
+    } else {
+        echo " Usuario não encontrado na base de dados";
+    }
+
+} else {
+    echo "Erro... Acesso Indevido!";
+}
 
 ?>
