9fe5e6d548c5840ec4bfaee077feb25b3996df5f
tatianetinoco <tatianeperr@gmail.com>
2025-05-28 00:37:25 -0300

    Criptografia Senha

--- projeto-nova-escola/valida_login.php
+++ projeto-nova-escola/valida_login.php
@@ -8,17 +8,20 @@ if(isset($_POST["acao"])){
     // Obtendo valores digitados no formulário
     $usuario = $_POST["usuario"];
     $senha   = $_POST["senha"];
+    $senha_hash = hash('sha256', $senha);
 
     // Verificar se existe na base de dados
-    $sql = "SELECT * FROM pessoas WHERE pessoas.cpf =:login and pessoas.senha = :senha";
+    $sql = "SELECT * FROM pessoas WHERE pessoas.cpf =:login AND pessoas.senha = :senha_hash";
     $comando = $pdo -> prepare($sql);
-    $comando -> bindParam(":login",$usuario);
-    $comando -> bindParam(":senha",$senha);
+    $comando -> bindParam(":login", $usuario);
+    $comando -> bindParam(":senha_hash", $senha_hash);
     $comando -> execute();
 
     if($comando -> rowCount()==1){
         echo " Existe um usuário em nossa base de dados ";
-        echo ($usuario);
+        echo ($usuario." <br> ");
+        echo ($senha. "<br>");
+        echo ("Senha hash ".hash("sha256",$senha));
     } else {
         echo " Usuario não encontrado na base de dados";
     }
