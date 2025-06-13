44890d762e30abbc33b9b00ff7224f7d5c6bcc5d
tatianetinoco <tatianeperr@gmail.com>
2025-06-12 22:50:50 -0300

    Atualização Formulário

--- projeto-nova-escola/dashboard/cadastrar_apagar.php
+++ projeto-nova-escola/dashboard/cadastrar_apagar.php
@@ -4,24 +4,52 @@ include_once("view/lateral.php");
 ?>
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-itens-center pt-3 pb-2 mb-3 border-bottom">
-        <form method="POST" action="processa/proc_cadastrar_pessoas.php">
+        <h1 class="h2">Cadastrar Pessoas</h1>
+    </div>        
+    
+    <form method="POST" action="processa/proc_cadastrar_pessoas.php">
+            
+            <div class="col-md-6">
+                <label for="nome" class="form-label">Nome:</label>
+                <input type="text" name="nome" class="form-control" placeholder="Digite seu Nome">
+                <div class="invalid-feedback">
+                    Por favor, insira o Nome.
+                </div>
+            </div>
+           
+            <div class="col-md-6 my-3">
+                <label for="cpf" class="form-label">CPF:</label>
+                <input type="text" name="cpf" class="form-control" placeholder="Digite seu CPF">
+                 <div class="invalid-feedback">
+                    Por favor, insira o CPF.
+                </div>
+            </div>
 
-            <label for="nome">Nome:</label><br>
-            <input type="text" name="nome"><br><br>
+            <div class="col-md-6 my-3">
+                <label for="senha" class="form-label">Senha:</label>
+                <input type="password" name="senha" class="form-control" placeholder="Digite sua Senha">
+                <div class="invalid-feedback">
+                    Por favor, insira a Senha.
+                </div>
+            </div>
 
-            <label for="cpf">CPF:</label><br>
-            <input type="text" name="cpf"><br><br>
-
-            <label for="senha">Senha:</label><br>
-            <input type="password" name="senha"><br><br>
-
-            <label for="tipo_user">Tipo Usuário:</label><br>
-            <input type="text" name="tipo_user"><br><br>
-
-            <label for="data_nasc">Data Nascimento:</label><br>
-            <input type="date" name="data_nasc"><br><br>
-
-            <input type="submit" class="btn btn-success" value="Cadastrar">
+            <div class="col-md-6 my-3">
+                <label for="tipo_user" class="form-label">Tipo Usuário:</label>
+                <input type="text" name="tipo_user" class="form-control" placeholder="Digite Tipo Usuário">
+                <div class="invalid-feedback">
+                    Por favor, insira o Tipo Usuário.
+                </div>
+            </div>
+           
+            <div class="col-md-2 my-3">
+                <label for="data_nasc" class="form-label">Data Nascimento:</label>
+                <input type="date" name="data_nasc" class="form-control">
+            </div>
+           
+             <div class="col-12 mt-5">
+                <button type="submit" class="btn btn-success">Cadastrar</button>
+                <a href="index_dash.php" class="btn btn-secondary">Voltar</a>
+            </div>
         </form>
     </div>
 </main>
